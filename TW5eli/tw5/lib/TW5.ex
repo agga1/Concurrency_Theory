75447e7bd52e64d5118e96cf910e3b81c55ef254
agga1 <agud100@gmail.com>
2020-11-06 14:43:16 +0100

    all working for long words (no 2^n compl), some comments

--- TW5eli/tw5/lib/TW5.ex
+++ TW5eli/tw5/lib/TW5.ex
@@ -12,14 +12,14 @@ defmodule TW5 do
     IOfunc.displaySet(dSet, "D")
     indSet =getIndependenceSet(dSet, alphabet)
     IOfunc.displaySet(indSet, "I")
-    foataClasses = foata(dSet, word)
-    IOfunc.displayFoata(foataClasses, wordName) # todo parse word name as well and dipslay it
+    foataClasses = getFoataFromSet(dSet, word)
+    IOfunc.displayFoata(foataClasses, wordName)
     letterIds = markLetters(word, 1)
     edges = createGraph(dSet, letterIds)
     foatafromGraph = getFoataFromGraph(edges, letterIds)
     IOfunc.displayFoata(foatafromGraph, wordName)
     IOfunc.saveAsDotFile(edges, letterIds)
-    # generate png representation of a graph using standard Graphviz library
+#     generate png representation of a graph using standard Graphviz library
     System.cmd("dot", [ "-Tpng", "haiku.dot", "-O"])
   end
 
@@ -48,7 +48,6 @@ defmodule TW5 do
 
   def getIndependenceSet(dSet, alphabet) do
     squaredSet = getSquareSet(alphabet, []) |> MapSet.new()
-    #    IO.puts("#{squaredSet}")
     MapSet.difference(squaredSet, dSet)
   end
 
@@ -59,22 +58,12 @@ defmodule TW5 do
     getSquareSet(tail, acc ++ [{el, el}|pairs])
   end
 
-  def foata(dSet, word) do
-    letter_class = getLetterClassnrPairs(dSet, word, [])
-    toFnF(letter_class)
-  end
-
-  def isDependent(deps, action1, action2) do
-    MapSet.member?(deps, {action1, action2})
+  def getFoataFromSet(dSet, word) do
+    getLetterClassnrPairs(dSet, word, []) |> toFnF()
   end
   
-  def getLetterClassnrPairs(_, [], processed_actions) do
-    processed_actions
-  end
-  
+  def getLetterClassnrPairs(_, [], processed_actions) do processed_actions end
   def getLetterClassnrPairs(deps, [letter | letters], processed_actions) do
-    # find min number from processed_actions filtered by dependency with letter
-    # recurse for letter and updated processed actions
     highestNr = processed_actions
           |> Enum.filter(fn({tmpLetter, _nr}) -> isDependent(deps, tmpLetter, letter) end)
           |> Enum.map(fn({_tmpLetter, nr})-> nr end)
@@ -82,6 +71,12 @@ defmodule TW5 do
     getLetterClassnrPairs(deps, letters, [{letter, highestNr+1} | processed_actions])
   end
 
+  # check if two actions are dependent
+  def isDependent(deps, action1, action2) do
+    MapSet.member?(deps, {action1, action2})
+  end
+
+  # change representation: e.g. from pairs (a, 2) (c, 1) (b, 2) to [[bc][a]]
   def toFnF(letter_class) do
     classN = letter_class |> Enum.reduce(0, fn {_letter, nr}, acc -> max(nr, acc) end)
     1..classN |> Enum.map(fn(nr) ->
@@ -89,23 +84,27 @@ defmodule TW5 do
       end)
   end
 
-  # assign id to each letter to distinguish between productions
+  # assign id to each letter to distinguish between productions (word aabc -> {a,1}, {a,2} {b,3} {c,4})
   def markLetters([], _) do [] end
   def markLetters([letter|letters], nr) do
     [{letter, nr}| markLetters(letters, nr+1)]
   end
 
+  @doc """
+  --GRAPH CREATION--
+  Create Diekert's graph from dependency set 'deps' and word as returned from 'markLetters'
+  returns graph as set of edges
+  """
   def createGraph(deps, letterIds) do
-    allEdgesSet = findAllEdges(deps, letterIds, []) |> MapSet.new()
-    n = length(letterIds)
-    redundant = findAllRedundant(allEdgesSet, [], 0, n, 0) |> MapSet.new()
-    edgeSet = MapSet.difference(allEdgesSet, redundant)
-    edgeSet
+    findAllEdges(deps, letterIds, [])
+      |> Enum.sort( fn({f1, t1}, {f2, t2})-> # sort edges for better complexity of removing redundant edges
+        if t1 == t2 do f1>=f2 else t1<=t2 end end )
+      |> removeRedundant([])
   end
 
+  # find all edges of dependency graph, analyzing letter by letter
   def findAllEdges(_, [], edges) do edges end
   def findAllEdges(deps, [letterId | letterIds], edges) do
-    # find all connections with future letters
     {letter, id} = letterId
     newEdges = letterIds
                |> Enum.filter(fn{tLetter, _} -> isDependent(deps, tLetter, letter) end)
@@ -113,43 +112,46 @@ defmodule TW5 do
     findAllEdges(deps, letterIds, edges ++ newEdges)
   end
 
-  def findAllRedundant(edges, prefix, n, n, ones) do
-    if ones>2 do
-      path = binaryToPath(prefix, n)
-      if pathExists?(edges, path) do
-        [{List.first(path), List.last(path)}] else [] end
-    else [] end
+  # returns a set of edges, where all edges are irreplacable by a subset of any other edges
+  def removeRedundant([], accepted) do accepted end
+  def removeRedundant([edge | edges], accepted) do
+    if isReplacable?(accepted, edge) do
+      removeRedundant(edges, accepted) # reject edge
+    else
+      removeRedundant(edges, [edge | accepted]) # accept edge
+    end
   end
-  def findAllRedundant(edges, prefix, len, n, ones) do
-    findAllRedundant(edges, [0|prefix], len+1, n, ones) ++ findAllRedundant(edges, [1|prefix], len+1, n, ones+1)
-  end
-  def binaryToPath(binary, n)do
-    List.zip([binary, 1..n|> Enum.to_list()])
-    |> Enum.filter(fn {bin, _id} -> bin==1 end)
-    |> Enum.map(fn {_bin, id} -> id end)
-  end
-  def pathExists?(_edges, []) do true end
-  def pathExists?(_edges, [_nr]) do true end
-  def pathExists?(edges, [nr1, nr2| rest]) do
-    MapSet.member?(edges, {nr1, nr2}) and pathExists?(edges, [nr2|rest])
+  # check recursively all combinations of already accepted edges, if they can replace current edge
+  def isReplacable?(accepted, {from, to}) do
+      Enum.any?(accepted, &(&1 == {from, to})) or
+      accepted |> Enum.filter(fn{_, t} -> t == to end)
+               |> Enum.any?(fn{f, _} -> isReplacable?(accepted, {from, f}) end)
   end
 
+  @doc """
+  --FOATA CLASSES FROM GRAPH--
+  In each step take all vertices (represented as tuple {letter, id} ) that have no incoming edges,
+  and mark them as new class.
+  """
   def getFoataFromGraph(_edges, []) do [] end
-  def getFoataFromGraph(edges, labelsIds) do
-    division = labelsIds |> Enum.group_by(fn{_letter, id} -> hasInputs(edges, id) end)
+  def getFoataFromGraph(edges, lettersIds) do
+    # split vertices into 2 groups depending on whether they have any input edges
+    division = lettersIds |> Enum.group_by(fn{_letter, id} -> hasInputs?(edges, id) end)
     newClass= Map.get(division, false, [])
     remaingingIds = Map.get(division, true, [])
-    newEdges =  deleteEdgesFromVertex(newClass, edges)
+    newEdges =  deleteEdgesFromVertices(newClass, edges)
     [ newClass |> Enum.map(&elem(&1, 0)) |  getFoataFromGraph(newEdges, remaingingIds)]
   end
 
-  def hasInputs(edges, id) do
-    1..(id-1) |> Enum.any?(&MapSet.member?(edges, {&1, id}))
+  # check whether vertex with id 'id' has any incoming edges
+  def hasInputs?(edges, id) do
+    edges |> Enum.any?(fn{_, to} -> to == id end)
   end
 
-  def deleteEdgesFromVertex([], edges) do edges end
-  def deleteEdgesFromVertex([{_letter, id}| vertices], edges) do
-    deleteEdgesFromVertex(vertices, edges |> Enum.filter(fn{from, _to}-> from != id end) |> MapSet.new())
+  # delete all edges from vertices that are already processed
+  def deleteEdgesFromVertices([], edges) do edges end
+  def deleteEdgesFromVertices([{_letter, id}| vertices], edges) do
+    deleteEdgesFromVertices(vertices, edges |> Enum.filter(fn{from, _}-> from != id end))
   end
 
 end
